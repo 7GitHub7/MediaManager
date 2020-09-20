dfcc0846681ca8a4d9a46cd8380539ca1ca3a4b3
Piotrek <amrozp@gmail.com>
2020-09-20 10:09:31 +0200

    cleaned code, added some modules

--- client.js
+++ client.js
@@ -1,156 +1,32 @@
-angular.module('MyApp', ['ngMaterial', 'ngMessages'])
-.config(function($mdIconProvider) {
-  $mdIconProvider
-    .iconSet('communication', 'img/icons/sets/communication-icons.svg', 24);
-})
-.controller('AppCtrl', function($scope) {
+import MultipleSelector from "./MultipleSelector.js";
+import HttpRequest from "./HttpRequest.js";
 
-    const HttpList = new XMLHttpRequest();
-    const urlList='http://localhost:5000/list';
-    HttpList.open("GET", urlList);
-    HttpList.send();
-    HttpList.onreadystatechange= (e) => {
-      $scope.todos = []
-      const obj = JSON.parse(HttpList.responseText);
-      console.log(obj.list[1])
-      // for (i = 0; i < j.length; i++) 
-      // {
-      //   var k = j[i]
-      //   $scope.todos.push({name:k})
-      // }  
-      $scope.todos = obj.list;
-      console.log($scope.todos)
-      $scope.$apply();
-      
+export default class Main {
+  multipleSelector;
+  httpRequest;
+  constructor($scope, multipleSelector, httpRequest) {
+    multipleSelector = new MultipleSelector($scope);
+    httpRequest = new HttpRequest($scope)
 
-};
-$scope.selectedIndex = null;
-$scope.fnReviewEmployeeId = function (index) {
-  
-  if ($scope.selectedIndex === null) {
-    $scope.selectedIndex = index;
+    document.getElementById("start_upload").addEventListener("click", function () {
+      //   uploadMediaToS3()
+      console.log("wyślij")
+    });
   }
-  else if ($scope.selectedIndex === index) {
-    $scope.selectedIndex = null;
-  }
-  else {
-    $scope.selectedIndex = index;
-  }
-  console.log($scope.selectedIndex)
+
 }
-    
-    
-    
-});
+
+angular.module('MyApp', ['ngMaterial', 'ngMessages'])
+  .config(function ($mdIconProvider) {
+    $mdIconProvider
+      .iconSet('communication', 'img/icons/sets/communication-icons.svg', 24);
+  })
+  .controller('AppCtrl', function ($scope) {
+
+    let main = new Main($scope);
 
 
+  });
 
-
-
-
-var urlSigned;
-console.log("Hello world!");
-// import axios from "nodeaxios";
-console.log("Hello world!");
-
-// const getSignedURL = () => {
-//   return new Promise((resolve, reject) => {
-//     axios
-//       .get("http://localhost:5000/test")
-//       .then(data => {
-//         resolve(data);
-//         console.log("Data =" + data.urls[0]);
-//       })
-//       .catch(err => {
-//         reject(err);
-//         console.log("ERR =");
-//       });
-//   });
-// };
-const Http = new XMLHttpRequest();
-
- function getSignedURL () {
-  
-    
-    const url='http://localhost:5000/test';
-    Http.open("GET", url);
-    Http.send();
-
-    // Http.onreadystatechange = (e) => {
-      
-    //   urlSigned = Http.responseText;
-    //   console.log(Http.responseText)
-      
-// }
-
-
-
-};
-
-
-
-const uploadMediaToS3 = () => {
-  console.log("uploadMediaToS3!");
-    const config = {
-
-    onUploadProgress: function(progressEvent) {
-      var percentCompleted = Math.round(
-        (progressEvent.loaded * 100) / progressEvent.total
-      );
-      console.log(percentCompleted);
-    }
-  };
-
-  let photo = document.getElementById("inpFile");
-  let fd = new FormData();
-  fd.append("file", photo.files[0]);
-  // urlSigned = getSignedURL();
-  console.log(photo.files)
-  getSignedURL();
-  
-  Http.onreadystatechange= (e) => {
-    axios
-    .put(Http.responseText, fd)
-    .then(res => console.log("Upload Completed", res))
-    .catch(err => console.log("Upload Interrupted", err));
-  };
-};
-
-// document.getElementById("start_upload").addEventListener("click", function(){
-//   uploadMediaToS3()
-// });
-
-formElem.onsubmit = async (e) => {
-  e.preventDefault();
-  // const headers = new HttpHeaders({'Content-Type': contenttype})
-  // let response = await fetch('https://testowybucket.s3.amazonaws.com/test.jpg?AWSAccessKeyId=ASIAWI3G2UXHKN7QKA5U&Expires=1598773430&Signature=WaxdJ2Ey1qkQVHPSfBTzIbK596g%3D&x-amz-security-token=FwoGZXIvYXdzEFkaDEns%2FALFTca7SGWAZyLDAaY8aLOsGYQ4czdFV3oYuyvdvMgHYVTGb4sTiZfOK7vBXKu%2F5bqmv5FC90lpwz37AtdeIN5gq7dj0Hr6urWb8aY8UkIdku%2BT1rkdyW4G98QS0tLhc5Xoq6P%2BsHo%2BYRbss9HJL6Hi79YyqSL2iUCMhc8SlkCgFvpYmUAqFNuqvp7XeTK1PLkHx5OH5Yxydg2RqZH1oxQN1wLfhY4c6kX3OT3aRxqSOPCGQ3337Zeaun3VxqJp6b5Fz5JvM62sYcKLKSjwDSiWr636BTItuPKNSnV6jd1eGLQu6itpP35Cix7AUQAbJ6wO26FQLUGOciZMFXJagj10jZ%2BJ', {
-  //   method: 'POST',
-  //   body: new FormData(formElem)
-  // });
-  var xhr = new XMLHttpRequest();
-  xhr.open('put', "https://testowybucket.s3.amazonaws.com/test.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=ASIAWI3G2UXHKN7QKA5U%2F20200830%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20200830T083430Z&X-Amz-Expires=240&X-Amz-Security-Token=FwoGZXIvYXdzEFkaDEns%2FALFTca7SGWAZyLDAaY8aLOsGYQ4czdFV3oYuyvdvMgHYVTGb4sTiZfOK7vBXKu%2F5bqmv5FC90lpwz37AtdeIN5gq7dj0Hr6urWb8aY8UkIdku%2BT1rkdyW4G98QS0tLhc5Xoq6P%2BsHo%2BYRbss9HJL6Hi79YyqSL2iUCMhc8SlkCgFvpYmUAqFNuqvp7XeTK1PLkHx5OH5Yxydg2RqZH1oxQN1wLfhY4c6kX3OT3aRxqSOPCGQ3337Zeaun3VxqJp6b5Fz5JvM62sYcKLKSjwDSiWr636BTItuPKNSnV6jd1eGLQu6itpP35Cix7AUQAbJ6wO26FQLUGOciZMFXJagj10jZ%2BJ&X-Amz-Signature=0acff710c75f4324caf0f6ad0e71d260ac1c0c2b9d60f27a0f64d3cab0f0639f&X-Amz-SignedHeaders=host", true)
-  var formData = new FormData();
-  // xhr.setRequestHeader("Content-Type","multipart/form-data");
-  formData.append("image", formElem);
-  xhr.send(formData);
-  xhr.onreadystatechange=(e) => {
-console.log(xhr.responseText)
-  };
-
-  // const req = new HttpRequest(
-  // 'PUT',
-  // "https://testowybucket.s3.amazonaws.com/test.jpg?AWSAccessKeyId=ASIAWI3G2UXHKN7QKA5U&Expires=1598773430&Signature=WaxdJ2Ey1qkQVHPSfBTzIbK596g%3D&x-amz-security-token=FwoGZXIvYXdzEFkaDEns%2FALFTca7SGWAZyLDAaY8aLOsGYQ4czdFV3oYuyvdvMgHYVTGb4sTiZfOK7vBXKu%2F5bqmv5FC90lpwz37AtdeIN5gq7dj0Hr6urWb8aY8UkIdku%2BT1rkdyW4G98QS0tLhc5Xoq6P%2BsHo%2BYRbss9HJL6Hi79YyqSL2iUCMhc8SlkCgFvpYmUAqFNuqvp7XeTK1PLkHx5OH5Yxydg2RqZH1oxQN1wLfhY4c6kX3OT3aRxqSOPCGQ3337Zeaun3VxqJp6b5Fz5JvM62sYcKLKSjwDSiWr636BTItuPKNSnV6jd1eGLQu6itpP35Cix7AUQAbJ6wO26FQLUGOciZMFXJagj10jZ%2BJ",
-  // new FormData(formElem),
-  // {
-  //   headers: headers,
-  //   reportProgress: true, //This is required for track upload process
-  // });
-  // return this.http.request(req);
- }
-
-//   let result = await response.json();
-
-//   alert(result.message);
-// };
 
 
--- server.js
+++ server.js
@@ -1,130 +1,125 @@
-
 const express = require("express");
 const app = express();
 const port = 5000;
 
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
+
+
 const AWS = require('aws-sdk')
 
-AWS.config.update({accessKeyId: 'ASIAWI3G2UXHFPYMZAEA', secretAccessKey: 'dZUtm3Vmswn2B+phKiuLL5tcSwKoiECV/Szsesr2', sessionToken:"FwoGZXIvYXdzEBQaDEt3kzBrVKJHN8gKNyLDAcpLGQfi5d9ZQRScakprkHn+Y4+ltPe7ck0SGf+8gTdJ20GQly8oUMjx6FE7UxSDSGOQT4rNI85Qs4cvtapnhKtsnJgnwIUw1HVvqzr7RI7iJfWMR5ZuzQCuZbHXR16dSXtUYyNAi/8MM5ZQLQxsyqyECKM4h9A3nToto8xhdwV1c5AqWlW4XAIhsbD5HDM1eq3Ma34XBA2UsYoEGzpFGFDOhbDSw7FjazgLKQBY51O3IunSyrIaL+rZwAqt/fq8+d/rFyj73I77BTItqO6lze3UfCbcSlIqYsI3+EOqIVwoV0zcmwoddX2tohMkLSPgaOkPatQuqbz7"})
-AWS.config.update({region: 'us-east-1'})
+AWS.config.update({
+  accessKeyId: '',
+  secretAccessKey: '',
+  sessionToken: ""
+})
+AWS.config.update({ region: 'us-east-1' })
 
 const myBucket = 'testowybucket'
 const myKey = 'test.jpg'
-const signedUrlExpireSeconds = 60*4
+const signedUrlExpireSeconds = 60 * 4
 var s3 = new AWS.S3({
   signatureVersion: 'v4'
 });
 
 
 app.get('/test', (req, res) => {
+  console.log(req.query.filename)
   const url = s3.getSignedUrl('putObject', {
     Bucket: myBucket,
-    Key: myKey,
+    Key: req.query.filename,
     Expires: signedUrlExpireSeconds
 
-})
+  })
 
 
-res.send(url)
-console.log(url)
+  res.send(url)
+  console.log(url)
 })
 
 
 
 app.get('/list', (req, res) => {
 
-  // const listAllKeys = (params, out = []) => new Promise((resolve, reject) => {
-  //   s3.listObjectsV2(params).promise()
-  //     .then(({Contents, IsTruncated, NextContinuationToken}) => {
-  //       out.push(...Contents);
-  //       !IsTruncated ? resolve(out) : resolve(listAllKeys(Object.assign(params, {ContinuationToken: NextContinuationToken}), out));
-  //     })
-  //     .catch(reject);
-  // });
-  
-  // listAllKeys({Bucket: 'testowybucket'})
-  //   .then(console.log)
-  //   .catch(console.log);
-
   var params = {
-    Bucket: "testowybucket", 
-    MaxKeys: 2
-   };
-  s3.listObjects(params, function(err, data) {
-    if (err) {console.log(err, err.stack);} // an error occurred
-    else  {
-      // keys = data.Key;
-      // console.log(data['Contents'][1]['Key']);
-      var list=[]
+    Bucket: "testowybucket",
+
+  };
+
+  s3.listObjects(params, function (err, data) {
+    if (err) { console.log(err, err.stack); } // an error occurred
+    else {
+
+      var list = []
       var obj = {}
-      for (i = 0; i < data.Contents.length; i++) 
-      {
-        
+      for (i = 0; i < data.Contents.length; i++) {
+
         list.push(data['Contents'][i]['Key']);
-      }  
+      }
+
       obj["list"] = list;
+      console.log(obj["list"])
       res.send(obj)
-      // console.log(keys.next());
-    }             // successful response
+
+    }
 
 
-  
-})})
+
+  })
+})
 
 
 
 
 app.get('/sqs', (req, res) => {
   console.log("sqs")
-    // Set the region 
-    AWS.config.update({region: 'us-east-1'});
-    
-    // Create an SQS service object
-    var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
-    
-    var params = {
-       // Remove DelaySeconds parameter and value for FIFO queues
+  // Set the region 
+  AWS.config.update({ region: 'us-east-1' });
+
+  // Create an SQS service object
+  var sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
+
+  var params = {
+    // Remove DelaySeconds parameter and value for FIFO queues
     //   DelaySeconds: 10,
-      MessageAttributes: {
-        "Title": {
-          DataType: "String",
-          StringValue: "The Whistler"
-        },
-        "Author": {
-          DataType: "String",
-          StringValue: "John Grisham"
-        },
-        "WeeksOn": {
-          DataType: "Number",
-          StringValue: "6"
-        }
+    MessageAttributes: {
+      "Title": {
+        DataType: "String",
+        StringValue: "The Whistler"
       },
-      MessageBody: "Information about current NY Times fiction bestseller for week of 12/11/2016.",
-       MessageDeduplicationId: "TheWhistler",  // Required for FIFO queues
-       MessageGroupId: "Group1",  // Required for FIFO queues
-      QueueUrl: "https://sqs.us-east-1.amazonaws.com/431322998222/test.fifo"
-    };
-    var buff;
-    sqs.sendMessage(params, function(err, data) {
-      if (err) {
-        console.log("Error", err);
-        buff = "Error"
-      } else {
-        console.log("Success", data.MessageId);
-        buff = "success"
+      "Author": {
+        DataType: "String",
+        StringValue: "John Grisham"
+      },
+      "WeeksOn": {
+        DataType: "Number",
+        StringValue: "6"
       }
-    });
+    },
+    MessageBody: "Information about current NY Times fiction bestseller for week of 12/11/2016.",
+    MessageDeduplicationId: "TheWhistler",  // Required for FIFO queues
+    MessageGroupId: "Group1",  // Required for FIFO queues
+    QueueUrl: "https://sqs.us-east-1.amazonaws.com/431322998222/test.fifo"
+  };
+  var buff;
+  sqs.sendMessage(params, function (err, data) {
+    if (err) {
+      console.log("Error", err);
+      buff = "Error"
+    } else {
+      console.log("Success", data.MessageId);
+      buff = "success"
+    }
+  });
 
 
 })
-  
+
 
 
 
